89553f79353921dbeb5a1506b315529230d490c4
FairuzShadmaniShishir <fsshishir95@gmail.com>
2022-05-15 20:37:55 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,149 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import cv2
+import os
+import seaborn as sns
+import pandas as pd
+from skimage.filters import sobel
+from skimage.feature import greycomatrix, greycoprops
+from skimage.measure import shannon_entropy
+from skimage.filters import roberts, sobel, scharr, prewitt
+from scipy import ndimage as nd
+import cv2
+import numpy as np
+from skimage import io, img_as_float
+from skimage.filters import gaussian, median
+from skimage.morphology import disk
+from skimage.restoration import denoise_bilateral
+from skimage.restoration import denoise_nl_means, estimate_sigma
+from skimage.restoration import denoise_tv_chambolle
+from sklearn import preprocessing
+import pickle
+
+SIZE = 128
+
+test_images = []
+test_labels = []
+path = []
+dff = pd.DataFrame()
+# for directory_path in glob.glob("cell_images/test/*"):
+for directory_path in glob.glob("D:/Host Competition/Host_test/*"):
+    fruit_label = directory_path.split("\\")[-1]
+    for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+        img = cv2.imread(img_path, 0)
+        img = cv2.resize(img, (SIZE, SIZE))
+        path.append(os.path.basename(img_path))
+        test_images.append(img)
+        test_labels.append(fruit_label)
+
+test_images = np.array(test_images)
+test_labels = np.array(test_labels)
+
+le = preprocessing.LabelEncoder()
+le.fit(test_labels)
+test_labels_encoded = le.transform(test_labels)
+
+
+def feature_extractor(dataset):
+    image_dataset = pd.DataFrame()
+    for image in range(dataset.shape[0]):  # iterate through each file
+        # print(image)
+
+        df = pd.DataFrame()  # Temporary data frame to capture information for each loop.
+        # Reset dataframe to blank after each loop.
+
+        img = dataset[image, :, :]
+        ################################################################
+        # START ADDING DATA TO THE DATAFRAME
+        # Full image
+        # GLCM = greycomatrix(img, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4])
+        GLCM = greycomatrix(img, [1], [0])
+        GLCM_Energy = greycoprops(GLCM, 'energy')[0]
+        df['Energy'] = GLCM_Energy
+        GLCM_corr = greycoprops(GLCM, 'correlation')[0]
+        df['Corr'] = GLCM_corr
+        GLCM_diss = greycoprops(GLCM, 'dissimilarity')[0]
+        df['Diss_sim'] = GLCM_diss
+        GLCM_hom = greycoprops(GLCM, 'homogeneity')[0]
+        df['Homogen'] = GLCM_hom
+        GLCM_contr = greycoprops(GLCM, 'contrast')[0]
+        df['Contrast'] = GLCM_contr
+
+        GLCM2 = greycomatrix(img, [3], [0])
+        GLCM_Energy2 = greycoprops(GLCM2, 'energy')[0]
+        df['Energy2'] = GLCM_Energy2
+        GLCM_corr2 = greycoprops(GLCM2, 'correlation')[0]
+        df['Corr2'] = GLCM_corr2
+        GLCM_diss2 = greycoprops(GLCM2, 'dissimilarity')[0]
+        df['Diss_sim2'] = GLCM_diss2
+        GLCM_hom2 = greycoprops(GLCM2, 'homogeneity')[0]
+        df['Homogen2'] = GLCM_hom2
+        GLCM_contr2 = greycoprops(GLCM2, 'contrast')[0]
+        df['Contrast2'] = GLCM_contr2
+
+        GLCM3 = greycomatrix(img, [5], [0])
+        GLCM_Energy3 = greycoprops(GLCM3, 'energy')[0]
+        df['Energy3'] = GLCM_Energy3
+        GLCM_corr3 = greycoprops(GLCM3, 'correlation')[0]
+        df['Corr3'] = GLCM_corr3
+        GLCM_diss3 = greycoprops(GLCM3, 'dissimilarity')[0]
+        df['Diss_sim3'] = GLCM_diss3
+        GLCM_hom3 = greycoprops(GLCM3, 'homogeneity')[0]
+        df['Homogen3'] = GLCM_hom3
+        GLCM_contr3 = greycoprops(GLCM3, 'contrast')[0]
+        df['Contrast3'] = GLCM_contr3
+
+        GLCM4 = greycomatrix(img, [0], [np.pi / 4])
+        GLCM_Energy4 = greycoprops(GLCM4, 'energy')[0]
+        df['Energy4'] = GLCM_Energy4
+        GLCM_corr4 = greycoprops(GLCM4, 'correlation')[0]
+        df['Corr4'] = GLCM_corr4
+        GLCM_diss4 = greycoprops(GLCM4, 'dissimilarity')[0]
+        df['Diss_sim4'] = GLCM_diss4
+        GLCM_hom4 = greycoprops(GLCM4, 'homogeneity')[0]
+        df['Homogen4'] = GLCM_hom4
+        GLCM_contr4 = greycoprops(GLCM4, 'contrast')[0]
+        df['Contrast4'] = GLCM_contr4
+
+        GLCM5 = greycomatrix(img, [0], [np.pi / 2])
+        GLCM_Energy5 = greycoprops(GLCM5, 'energy')[0]
+        df['Energy5'] = GLCM_Energy5
+        GLCM_corr5 = greycoprops(GLCM5, 'correlation')[0]
+        df['Corr5'] = GLCM_corr5
+        GLCM_diss5 = greycoprops(GLCM5, 'dissimilarity')[0]
+        df['Diss_sim5'] = GLCM_diss5
+        GLCM_hom5 = greycoprops(GLCM5, 'homogeneity')[0]
+        df['Homogen5'] = GLCM_hom5
+        GLCM_contr5 = greycoprops(GLCM5, 'contrast')[0]
+        df['Contrast5'] = GLCM_contr5
+
+        GLCM6 = greycomatrix(img, [0], [np.pi / 6])
+        GLCM_Energy6 = greycoprops(GLCM6, 'energy')[0]
+        df['Energy6'] = GLCM_Energy6
+        GLCM_corr6 = greycoprops(GLCM6, 'correlation')[0]
+        df['Corr6'] = GLCM_corr6
+        GLCM_diss6 = greycoprops(GLCM6, 'dissimilarity')[0]
+        df['Diss_sim6'] = GLCM_diss6
+        GLCM_hom6 = greycoprops(GLCM6, 'homogeneity')[0]
+        df['Homogen6'] = GLCM_hom6
+        GLCM_contr6 = greycoprops(GLCM6, 'contrast')[0]
+        df['Contrast6'] = GLCM_contr6
+
+        # Append features from current image to the dataset
+        image_dataset = image_dataset.append(df)
+
+    return image_dataset
+
+
+test_features = feature_extractor(test_images)
+test_features = np.expand_dims(test_features, axis=0)
+test_for_RF = np.reshape(test_features, (test_images.shape[0], -1))
+
+model = pickle.load(open('model.pkl', 'rb'))
+test_prediction = model.predict(test_for_RF)
+test_prediction = le.inverse_transform(test_prediction)
+dff['Id']=path
+dff['label']=test_prediction
+print(test_prediction)
+dff.to_excel("Test_Prediction.xlsx")
